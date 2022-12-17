0aae152ce3288ae918750683e41f15b12fa246e2
MrCrackspider <36894028+MrCrackspider@users.noreply.github.com>
2022-12-17 16:53:24 +0300

    polishing

--- SQLServer2XML/Program.cs
+++ SQLServer2XML/Program.cs
@@ -7,48 +7,56 @@ if (args.Length < 3)
     Console.WriteLine("At least 3 arguments required");
     Environment.Exit(1);
 }
-//"Server = localhost; Database = BikeStores; Trusted_Connection = True;"
-//"Server = localhost; Database = BikeStoresEmpty; Trusted_Connection = True;"
-string ConnectionString = args[0];
-string Table = args[1]; //"sales.orders"
-string Mode = args[2].ToLower(); //"xml2db"
+
+string ConnectionString = args[0];  //"Server = localhost; Database = BikeStores; Trusted_Connection = True;"
+string Table = args[1];             //"sales.orders"
+string Mode = args[2].ToLower();    //"xml2db"
 string PathToFile = ".\\";
+if (args.Length > 3) PathToFile = args[3];
+
 try
 {
     if (Mode == "xml2db")
     {
         if (args.Length < 4)
         {
-            Console.WriteLine("Path to XML file required");
+            Console.WriteLine("Path to XML file is required");
             Environment.Exit(1);
         }
         else
         {
-            PathToFile = args[3];
-            DataTable FromXML = new DataTable(Table);
-            FromXML.ReadXml(PathToFile);
-            SqlConnection connection = new SqlConnection(ConnectionString);
-            connection.Open();
-
-            SqlBulkCopy BulkCopy = new SqlBulkCopy(connection);
-            foreach (DataColumn c in FromXML.Columns)
-                BulkCopy.ColumnMappings.Add(c.ColumnName, c.ColumnName);
-            BulkCopy.DestinationTableName = FromXML.TableName;
-            BulkCopy.WriteToServer(FromXML);
-            connection.Close();
+            DataTable XMLTable = new DataTable(Table);
+            XMLTable.ReadXml(PathToFile);
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlBulkCopy BulkCopy = new SqlBulkCopy(connection);
+                foreach (DataColumn c in XMLTable.Columns)
+                    BulkCopy.ColumnMappings.Add(c.ColumnName, c.ColumnName);
+                BulkCopy.DestinationTableName = XMLTable.TableName;
+                BulkCopy.WriteToServer(XMLTable);
+            }
         }
     }
     else if (Mode == "db2xml")
     {
-        SqlConnection connection = new SqlConnection(ConnectionString);
-        connection.Open();
-        string QueryStatement = "select top 20 * from " + Table + ";";
-        SqlCommand Command = new SqlCommand(QueryStatement, connection);
-        SqlDataAdapter DataAdapter = new SqlDataAdapter(Command);
-        DataTable BikeStores = new DataTable(Table);
-        DataAdapter.Fill(BikeStores);
-        connection.Close();
-        BikeStores.WriteXml(PathToFile + Table.Replace(".", "") + ".xml", XmlWriteMode.WriteSchema);
+        using (SqlConnection connection = new SqlConnection(ConnectionString))
+        {
+            string QueryStatement = "select * from " + Table + ";";
+            connection.Open();
+            SqlCommand Command = new SqlCommand(QueryStatement, connection);
+            DataTable BikeStores = new DataTable(Table);
+            SqlDataAdapter DataAdapter = new SqlDataAdapter(Command);
+            DataAdapter.Fill(BikeStores);
+            if (args.Length > 3)
+            {
+                BikeStores.WriteXml(PathToFile, XmlWriteMode.WriteSchema);
+            }
+            else
+            {
+                BikeStores.WriteXml(PathToFile + Table.Replace(".", "") + ".xml", XmlWriteMode.WriteSchema);
+            }
+        }
     }
     else Console.WriteLine("3 argument is incorrect");
 }
